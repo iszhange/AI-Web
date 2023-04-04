4ab74eb5dfe6ccc95d8081cb81cf95b88885819f
zhangkaizhuang <kenphp@yeah.net>
2023-04-04 11:17:58 +0800

    feat(项目): login page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import router from './router'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+import './utils/axios.js';
+import './style.css';
+import App from './App.vue';
 
 
 createApp(App).use(router).mount('#app');
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8 " style="border: 1px solid red;">
+  <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8 w-96">
     <div class="w-full max-w-md space-y-8">
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-        </p>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="AI Workstation Web">
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">AIWW</h2>
+<!--        <p class="mt-2 text-center text-sm text-gray-600">-->
+<!--          Or-->
+<!--          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>-->
+<!--        </p>-->
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true">
@@ -28,23 +28,23 @@
 
         <div class="flex items-center justify-between">
           <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+<!--            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">-->
+<!--            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>-->
           </div>
 
           <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">忘记密码?</a>
           </div>
         </div>
 
         <div>
           <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-            </svg>
-          </span>
-            Sign in
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            登录
           </button>
         </div>
       </form>
